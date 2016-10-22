8488c6d0343ce3d483f4113bca5474dc31311c93
AlexTilinina <sasha_til@mail.ru>
2016-10-22 20:04:09 +0300

    Added speed change + points counter + buttons(fix it)

--- Eggs.java
+++ Eggs.java
@@ -6,34 +6,78 @@ import java.awt.geom.Ellipse2D;
 import java.util.Random;
  
 public class Eggs extends JComponent {
-    private int x1;
+    private int x1 = 100, b, xw = 250;
 	private int y = 50;
+	private int i = 0;
+	private boolean game = true;
+    private int [] x = new int[] {100, 250, 400};
+	
+	public boolean getGame(){
+		return game;
+	}
+	
+	public boolean setGame(boolean g){
+		this.game = g;
+		return game;
+	}
+	
+	public int setI(int i){
+		this.i = 0;
+		return i;
+	}
  
     public Eggs() {
-		Random r = new Random();
-        int []x = new int[] {100, 250, 500};
+		
+        
         Timer timer = new Timer(30, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-				if (y > 450) {
-					x1 = x[r.nextInt(x.length)];
+				Random r = new Random();
+				if (y > 350) {
 					y = 50;
+					if (x1 == xw) {
+						i++;
+					}
+					else {
+						game = false;
+					}
+					x1 = x[r.nextInt(x.length)];
 				}
-                repaint();
+				if ((b == 39) && (xw < 400))
+					xw += 150;
+				if ((b == 37) && (xw > 100))
+					xw = xw - 150;
+				b = 0;
+				repaint();
             }
         });
         timer.start();
+    }
+
+    void setXW(int b){
+        this.b = b;
     }
  
     @Override
     protected void paintComponent(Graphics g) {
 		int width = getWidth();
         int height = getHeight();
-		g.setColor(Color.blue);
+		setBackground(new Color(168, 188, 255));
+		g.setColor(new Color(168, 188, 255));
         g.fillRect(0, 0, width, height);
-        Graphics2D g2d = (Graphics2D) g;
-		g2d.setPaint(Color.white);
-		g.fillOval(x1, y, 75, 125);
-		y += 5;
+		g.setFont(new Font("Arial", 1, 20));
+		if (game) {
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setPaint(Color.white);
+			g.fillRect(xw, 400, 100, 70);
+			g.fillOval(x1, y, 75, 125);
+			y += 5 + i / 3;
+			g.setColor(new Color(0, 0, 0));
+		}
+		else {
+			g.setColor(new Color(0, 0, 0));
+			g.drawString("Game over", width / 2 - 50, height/ 2);
+		}
+		g.drawString("" + (i * 10), width - 50, height - 50);
 	}
 }
--- Wolf.java
+++ Wolf.java
@@ -1,22 +1,54 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.applet.Applet;
+import java.util.*;
 
-public class Wolf {	
+public class Wolf  {	
 	
-	
-	public static void main(String[] args){
-		
+		public static void main(String[] args){
+			
 		SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 JFrame frame = new JFrame("Wolf");
-                frame.add(new Eggs());
+                final Eggs cort = new Eggs();
+                frame.add(cort);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setSize(700, 500);
+                frame.addKeyListener(new KeyAdapter() {
+					public void keyPressed(KeyEvent e) {
+						cort.setXW (e.getKeyCode());
+                    }
+				});
                 frame.setVisible(true);
+				/*JButton tryAgain = new JButton("Try again");
+				JButton exit = new JButton("Exit");
+				frame.add(exit);
+				frame.add(tryAgain);
+				exit.setVisible(!cort.getGame());
+				tryAgain.setVisible(!cort.getGame());
+				exit.setBounds(cort.getWidth()/2, cort.getHeight()/2, 100, 30); 
+				tryAgain.setBounds(cort.getWidth()/2 - 150, cort.getHeight()/2, 100, 30); 
+				exit.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e) {
+						if (e.getActionCommand().equals("Exit")) {
+							System.exit(0);
+						}
+					}
+				});
+				tryAgain.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e) {
+						if (e.getActionCommand().equals("Try again")) {
+							cort.setGame(true);
+							cort.setI(0);
+						}
+					}
+				});*/
             }
         });
+       
 		
 	}
+  
 }
